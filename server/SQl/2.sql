25f2276c0dec132a73173233ab70b91d3a2bf5da
LinZexiao <812065654@qq.com>
2021-02-23 20:52:14 +0800

    add sql

---
+++ server/SQl/2.sql
@@ -0,0 +1,48 @@
+-- 更新 学生 或者 课程信息
+
+USE DBforpProject
+GO
+
+SELECT *
+FROM SC
+GO
+
+-- 修改成绩
+-- Update rows in table 'SC'
+UPDATE SC
+SET
+grade = 91
+    -- [Colum1] = Colum1_Value,
+    -- [Colum2] = Colum2_Value
+    -- add more columns and values here
+WHERE sno = 1107 AND cno = 	8305001 
+GO
+
+
+
+-- 修改其他信息
+-- 插入删除
+
+INSERT into SC
+    (sno,cno,grade)
+VALUES
+    (1107 , 8305002, 99)
+
+SELECT *
+FROM SC
+WHERE sno
+= 1107
+GO
+
+
+
+
+DELETE from SC
+WHERE sno
+= 1107 AND cno = 	8305002
+
+SELECT *
+FROM SC
+WHERE sno
+= 1107
+GO
